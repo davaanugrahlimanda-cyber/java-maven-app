125388ad3a9e98de6e5d73bcdc3e84948d2910b4
BangDav <davaanugrahlimanda@gmail.com>
2025-10-03 16:43:05 +0700

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
+
     @Test
     public void testApp() {
         assertTrue(true, "This test should always pass");
